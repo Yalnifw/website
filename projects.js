4cc6d7cfa37101601fc2184bbd9f41bd78038d65
William Finlay <william@johns-mbp.lan>
2021-04-29 09:16:56 -0500

    ProjectInfrastructureV1

--- projects.js
+++ projects.js
@@ -1,3 +1,11 @@
+var current = document.getElementById("default");
+
 function sample(){
     document.getElementById("viewport").innerHTML = "<br><em>It Worked!</em>";
+}
+
+function setContent(prj){
+    current.style.display = "none";
+    current = document.getElementById(prj);
+    current.style.display = "block";
 }
